ad623ca137644ede893b2d4d87361bd7cdb4be13
Vitaliy <vitaliy.zayakin@gmail.com>
2023-04-28 17:54:38 +0500

    feat: add styles to keyboard

--- src/index.js
+++ src/index.js
@@ -1,29 +1,38 @@
 import keys from './keys.js';
 
 window.addEventListener('DOMContentLoaded', () => {
-  const div = document.createElement('div');
-  div.classList.add('container');
+  const container = document.createElement('div');
+  const keyboard = document.createElement('div');
+
+  container.classList.add('container');
+  keyboard.classList.add('keyboard');
 
   Object.entries(keys).forEach(([key, value]) => {
     const button = document.createElement('button');
     button.classList.add('keyboard__key', key);
     button.textContent = value;
-    div.appendChild(button);
+    keyboard.appendChild(button);
   });
-
-  document.body.appendChild(div);
+  container.appendChild(keyboard);
+  document.body.appendChild(container);
 
   window.addEventListener('keydown', (e) => {
     e.preventDefault();
     const { code } = e;
     const button = document.querySelector(`.${code}`);
-    button.classList.add('active');
+    button.classList.add('keyboard__key_active');
   });
 
   window.addEventListener('keyup', (e) => {
     e.preventDefault();
     const { code } = e;
     const button = document.querySelector(`.${code}`);
-    button.classList.remove('active');
+    button.classList.remove('keyboard__key_active');
+  });
+
+  window.addEventListener('blur', () => {
+    document
+      .querySelectorAll('.keyboard__key')
+      .forEach((key) => key.classList.remove('keyboard__key_active'));
   });
 });
