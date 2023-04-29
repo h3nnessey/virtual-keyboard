46f10b71cec16eaede81a483350d5735548c7e8f
Vitaliy <vitaliy.zayakin@gmail.com>
2023-04-29 14:33:21 +0500

    feat: add Keyboard class proto

--- src/index.js
+++ src/index.js
@@ -1,38 +1,46 @@
 import keys from './keys.js';
 
+class Keyboard {
+  static init() {
+    this.container = document.createElement('div');
+    this.keyboard = document.createElement('div');
+    this.container.classList.add('container');
+    this.keyboard.classList.add('keyboard');
+    this.keyboard.appendChild(this.createKeyboardKeys(keys));
+    this.container.appendChild(this.keyboard);
+    document.body.appendChild(this.container);
+
+    window.addEventListener('keydown', (e) => {
+      const button = this.getPressedKey(e);
+      button.classList.add('keyboard__key_active');
+    });
+
+    window.addEventListener('keyup', (e) => {
+      const button = this.getPressedKey(e);
+      button.classList.remove('keyboard__key_active');
+    });
+  }
+
+  static createKeyboardKeys(keysObject) {
+    const documentFragment = document.createDocumentFragment();
+
+    Object.entries(keysObject).forEach(([key, value]) => {
+      const button = document.createElement('button');
+      button.classList.add('keyboard__key', key);
+      button.textContent = value;
+      documentFragment.appendChild(button);
+    });
+
+    return documentFragment;
+  }
+
+  static getPressedKey(e) {
+    e.preventDefault();
+    const { code } = e;
+    return document.querySelector(`.${code}`);
+  }
+}
+
 window.addEventListener('DOMContentLoaded', () => {
-  const container = document.createElement('div');
-  const keyboard = document.createElement('div');
-
-  container.classList.add('container');
-  keyboard.classList.add('keyboard');
-
-  Object.entries(keys).forEach(([key, value]) => {
-    const button = document.createElement('button');
-    button.classList.add('keyboard__key', key);
-    button.textContent = value;
-    keyboard.appendChild(button);
-  });
-  container.appendChild(keyboard);
-  document.body.appendChild(container);
-
-  window.addEventListener('keydown', (e) => {
-    e.preventDefault();
-    const { code } = e;
-    const button = document.querySelector(`.${code}`);
-    button.classList.add('keyboard__key_active');
-  });
-
-  window.addEventListener('keyup', (e) => {
-    e.preventDefault();
-    const { code } = e;
-    const button = document.querySelector(`.${code}`);
-    button.classList.remove('keyboard__key_active');
-  });
-
-  window.addEventListener('blur', () => {
-    document
-      .querySelectorAll('.keyboard__key')
-      .forEach((key) => key.classList.remove('keyboard__key_active'));
-  });
+  Keyboard.init();
 });
